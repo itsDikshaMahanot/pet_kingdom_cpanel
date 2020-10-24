d4a42333b45dd7555a7f6f9ee9cd54c97785e987
Diksha Mahanot <59890161+itsDikshaMahanot@users.noreply.github.com>
2020-10-24 17:52:34 +0530

    Page 2 insert done

--- lib/insert_screen.dart
+++ lib/insert_screen.dart
@@ -5,9 +5,119 @@ class InsertScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.amberAccent,
-      child: Text("Hello"),
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: Stack(
+        children: <Widget>[
+          Expanded(
+            child: SafeArea(
+              child: Container(
+                margin: EdgeInsets.only(top: 150),
+                child: GridView.count(
+                  crossAxisCount: 2,
+                  primary: false,
+                  mainAxisSpacing: 40,
+                  crossAxisSpacing: 10,
+                  children: <Widget>[
+                    Card(
+                      margin: EdgeInsets.all(10),
+                      child: Column(
+                        children: [
+                          Image.network(
+                            "https://cdn.onlinewebfonts.com/svg/img_498109.png",
+                            height: 150,
+                            width: 180,
+                          ),
+                          Text(
+                            "Fish",
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          )
+                        ],
+                      ),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)
+                     ),
+                     elevation: 5,
+                    ),
+                    Card(
+                      margin: EdgeInsets.all(10),
+                      child: Column(
+                        children: [
+                          Image.network(
+                            "https://www.vhv.rs/viewpic/hxJbRJJ_dog-vector-dog-png-vector-transparent-png",
+                            height: 150,
+                            width: 180,
+                          ),
+                          Text(
+                            "Dog",
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          )
+                        ],
+                      ),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)
+                      ),
+                       elevation: 5,
+                    ),
+                    Card(
+                      margin: EdgeInsets.all(10),
+                      child: Column(
+                        children: [
+                          Image.network(
+                            "https://cdn.onlinewebfonts.com/svg/img_498109.png",
+                            height: 150,
+                            width: 180,
+                          ),
+                          Text(
+                            "Bird",
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          )
+                        ],
+                      ),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)
+                      ),
+                       elevation: 5,
+                    ),
+                    Card(
+                      margin: EdgeInsets.all(10),
+                      child: Column(
+                        children: [
+                          Image.network(
+                            "https://cdn.onlinewebfonts.com/svg/img_498109.png",
+                            height: 150,
+                            width: 180,
+                          ),
+                          Text(
+                            "Plants",
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          )
+                        ],
+                      ),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)
+                      ),
+                       elevation: 5,
+                    )
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
